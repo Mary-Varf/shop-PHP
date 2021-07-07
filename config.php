67b7f051927e715a9e29ebb784acf2321edcf9bd
Mary-Varf <67269361+Mary-Varf@users.noreply.github.com>
2021-07-07 13:25:15 +0200

    deleted commented lines
    deleted commented lines

--- config.php
+++ config.php
@@ -4,10 +4,6 @@ define('HOST', 'a355932.mysql.mchost.ru');
 define('USER', 'a355932_mydb');
 define('PASSWORDSQL', 'a355932mydb');
 define('DBNAME', 'a355932_mydb');
-// define('HOST', 'ivan');
-// define('USER', 'root');
-// define('PASSWORDSQL', 'root');
-// define('DBNAME', 'mydb');
 
 define('FREE_DEL_PRICE', 2000);
 define('AD_DEL_PRICE', 280);
